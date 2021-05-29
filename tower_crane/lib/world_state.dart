85b6d0496035dbb8876dc4410754774071fbe696
Andrew Vaimon <niko142000@mail.ru>
2021-05-29 12:51:52 +0300

    физика

--- tower_crane/lib/world_state.dart
+++ tower_crane/lib/world_state.dart
@@ -4,4 +4,7 @@ class WorldState{
   static double shipX = 585.0, shipY = 200.0, shipZ = 278.0;
   static double waveZ = 468.0;
   static Function(double) waveFunction = WaveFunctionsHelper.functions[0];
+  static double windDirection = 0;
+  static double windSpeed = 0;
+  static int ropeLength = 40;
 }
