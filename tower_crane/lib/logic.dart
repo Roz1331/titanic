26e74ae2878672ef2cf0707a73e59190e4d1386c
Rose <ismailova_roza_1@mail.ru>
2021-05-30 13:43:23 +0300

    физика оживает

---
+++ tower_crane/lib/logic.dart
@@ -0,0 +1,17 @@
+import 'package:tower_crane/world_state.dart';
+import 'package:tower_crane/stupid_constants.dart';
+// import 'dart:math';
+
+class Logic {
+  static void carriageTDownVelocity(Function operand) {
+    double carriageToShipDistance = WorldState.shipZ -
+        WorldState.boxPlaces[WorldState.currentTarget] *
+            ContainerBoxDimensions.height -
+        WorldState.containerBoxZ;
+    double time = 0;
+
+    while (carriageToShipDistance >= 1) {
+      time += 0.1;
+    }
+  }
+}
